530423e74ad13289d279c086551e96f109e90b94
sai-charan-123 <saicharan271305@gmail.com>
2026-05-30 12:20:49 +0530

    operators_programs

--- AM_operator_1.py
+++ AM_operator_1.py
@@ -1,6 +1,13 @@
-s=30
+#method 1
+'''s=30
 c=50
 g=s+c
 print("value of S :",s)
 print("value of C :",c)
 print("value of G :",g)
+'''
+#method 2
+a=int(input("Enter value of A: "))
+b=int(input("Enter value of B: "))
+c=a+b
+print(f" value of A : {a}\n value of B : {b}\n value of C : {c}")
